c9db95a205bc16af0ff8fb35f4fcd6b27ce10372
Nathan Moats <nathanmoats@outlook.com>
2017-09-26 19:11:14 -0600

    initial commit

---
+++ client/src/components/Bin.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Bin extends Component {
+  render() {
+    return (
+      <div>
+        <p>Bin Test</p>
+      </div>
+    );
+  }
+}
+
+export default Bin;
